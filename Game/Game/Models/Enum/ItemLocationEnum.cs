ad6974c1824cdc2e40efb958f72b4213f89e4ec9
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-01-25 21:35:49 -0800

    Changed the name of Item from Necklass to Necklace in ItemLocationEnum.cs

--- Game/Game/Models/Enum/ItemLocationEnum.cs
+++ Game/Game/Models/Enum/ItemLocationEnum.cs
@@ -18,7 +18,7 @@ namespace Game.Models
         Head = 10,
 
         // Things to put around the neck, such as necklass, broaches, scarfs, neck ribbons.  Can have at the same time with Head items ex. Ribbon for Hair, and Ribbon for Neck is OK to have
-        Necklass = 12,
+        Necklace = 12,
 
         // The primary hand used for fighting with a sword or a staff.  
         PrimaryHand = 20,
