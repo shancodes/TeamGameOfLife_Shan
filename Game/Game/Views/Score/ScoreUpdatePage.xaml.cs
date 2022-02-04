b7994af8991a3148a9fd33b4a7ba72339963304e
Shanmugapriya Ravi <sravi@seattleu.edu>
2022-02-03 20:54:06 -0800

    Added Reset Functionality for Update Score page in ScoreUpdatePage.xaml and ScoreUpdatePage.xaml.cs

--- Game/Game/Views/Score/ScoreUpdatePage.xaml.cs
+++ Game/Game/Views/Score/ScoreUpdatePage.xaml.cs
@@ -18,6 +18,7 @@ namespace Game.Views
     {
         // View Model for Score
         public readonly GenericViewModel<ScoreModel> ViewModel;
+        private ScoreModel OriginalModel;
 
         // Constructor for Unit Testing
         public ScoreUpdatePage(bool UnitTest) { }
@@ -28,6 +29,7 @@ namespace Game.Views
         public ScoreUpdatePage(GenericViewModel<ScoreModel> data)
         {
             InitializeComponent();
+            this.OriginalModel = new ScoreModel(data.Data);
 
             BindingContext = this.ViewModel = data;
 
@@ -46,6 +48,16 @@ namespace Game.Views
         }
 
         /// <summary>
+        /// Helper Function that updates all the fields with current values from ViewModel.Data
+        /// </summary>
+        private void UpdateUIElements()
+        {
+            Name_Entry.Text = ViewModel.Data.Name.ToString();
+            Score_Entry.Text = ViewModel.Data.ScoreTotal.ToString();
+            GameTime_Entry.Text = ViewModel.Data.GameDate.ToString();
+        }
+
+        /// <summary>
         /// Cancel and close this page
         /// </summary>
         /// <param name="sender"></param>
@@ -53,6 +65,18 @@ namespace Game.Views
         public async void Cancel_Clicked(object sender, EventArgs e)
         {
             _ = await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Reset the fields to original values before update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Reset_Clicked(object sender, EventArgs e)
+        {
+            this.ViewModel.Data.Update(OriginalModel);
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+            UpdateUIElements();
         }
     }
 }
