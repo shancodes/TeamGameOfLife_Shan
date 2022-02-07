b3592145de2d84098dba76e8baf2dff782855dc4
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-02-07 14:30:53 -0800

    Added unit test ItemUpdatePage_OnResetButtonPressed_Valid_Should_Pass in ItemUpdatePageTests.cs

--- UnitTests/Views/Items/ItemUpdatePageTests.cs
+++ UnitTests/Views/Items/ItemUpdatePageTests.cs
@@ -173,5 +173,19 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void ItemUpdatePage_OnResetButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Reset_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
