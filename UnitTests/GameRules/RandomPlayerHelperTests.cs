f1586ce82330914e0879aeef0100e1cf2b9b3d45
Shanmugapriya Ravi <sravi@seattleu.edu>
2022-02-09 11:41:35 -0800

    Added Unit tests after changing Monster Name and Image Source in RandomPlayerHelperTests.cs

--- UnitTests/GameRules/RandomPlayerHelperTests.cs
+++ UnitTests/GameRules/RandomPlayerHelperTests.cs
@@ -77,7 +77,7 @@ namespace UnitTests.Helpers
             _ = DiceHelper.DisableForcedRolls();
 
             // Assert
-            Assert.AreEqual("Deg", result);
+            Assert.AreEqual("Ice Jester", result);
         }
 
         [Test]
@@ -193,7 +193,7 @@ namespace UnitTests.Helpers
             _ = DiceHelper.DisableForcedRolls();
 
             // Assert
-            Assert.AreEqual("troll2.png", result);
+            Assert.AreEqual("ice_jester.png", result);
         }
 
         [Test]
