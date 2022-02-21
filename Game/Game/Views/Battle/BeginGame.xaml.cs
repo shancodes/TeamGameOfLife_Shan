816f32a2d866f0c3f9baeaebd0bfdd17e438899f
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-21 13:49:54 -0800

    Added new page BeginGame.xaml, BeginGame.xaml.cs. Changes done in GamePage.xaml.cs, BeginGame.xaml.cs, BeginGame.xaml, Game.csproj

---
+++ Game/Game/Views/Battle/BeginGame.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Main Game Page
+    /// </summary>
+    
+    public partial class BeginGame : ContentPage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BeginGame()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
