7ea22185d3e79df1a0caa0a4deefb0ec14b745d1
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-24 15:32:33 -0800

    Removed the commented out code from RoundOverPage.xaml.cs

--- Game/Game/Views/Battle/RoundOverPage.xaml.cs
+++ Game/Game/Views/Battle/RoundOverPage.xaml.cs
@@ -21,20 +21,10 @@ namespace Game.Views
         {
             InitializeComponent();
 
-            // Update the Round Count
-            //TotalRound.Text = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.RoundCount.ToString();
-
-            // Update the Found Number
-            //TotalFound.Text = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelDropList.Count().ToString();
-
-            // Update the Selected Number, this gets updated later when selected refresh happens
-            //TotalSelected.Text = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelSelectList.Count().ToString();
-
             DrawCharacterList();
 
             DrawMonsterList();
 
-            //DrawItemLists();
         }
 
         /// <summary>
@@ -125,116 +115,6 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// Draw the List of Items
-        /// 
-        /// The Ones Dropped
-        /// 
-        /// The Ones Selected
-        /// 
-        /// </summary>
-        //public void DrawItemLists()
-        //{
-        //DrawDroppedItems();
-        //DrawSelectedItems();
-
-        // Only need to update the selected, the Dropped is set in the constructor
-        //TotalSelected.Text = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelSelectList.Count().ToString();
-        //}
-
-        /// <summary>
-        /// Add the Dropped Items to the Display
-        /// </summary>
-        //public void DrawDroppedItems()
-        //{
-        //    // Clear and Populate the Dropped Items
-        //    var FlexList = ItemListFoundFrame.Children.ToList();
-        //    foreach (var data in FlexList)
-        //    {
-        //        _ = ItemListFoundFrame.Children.Remove(data);
-        //    }
-
-        //    foreach (var data in BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelDropList.Distinct())
-        //    {
-        //        ItemListFoundFrame.Children.Add(GetItemToDisplay(data));
-        //    }
-        //}
-
-        /// <summary>
-        /// Add the Dropped Items to the Display
-        /// </summary>
-        //public void DrawSelectedItems()
-        //{
-        //    // Clear and Populate the Dropped Items
-        //    var FlexList = ItemListSelectedFrame.Children.ToList();
-        //    foreach (var data in FlexList)
-        //    {
-        //        _ = ItemListSelectedFrame.Children.Remove(data);
-        //    }
-
-        //    foreach (var data in BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelSelectList)
-        //    {
-        //        ItemListSelectedFrame.Children.Add(GetItemToDisplay(data));
-        //    }
-        //}
-
-        /// <summary>
-        /// Look up the Item to Display
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        //public StackLayout GetItemToDisplay(ItemModel item)
-        //{
-        //    if (item == null)
-        //    {
-        //        return new StackLayout();
-        //    }
-
-        //    if (string.IsNullOrEmpty(item.Id))
-        //    {
-        //        return new StackLayout();
-        //    }
-
-        //    // Defualt Image is the Plus
-        //    var ClickableButton = true;
-
-        //    var data = ItemIndexViewModel.Instance.GetItem(item.Id);
-        //    if (data == null)
-        //    {
-        //        // Show the Default Icon for the Location
-        //        data = new ItemModel { Name = "Unknown", ImageURI = "icon_cancel.png" };
-
-        //        // Turn off click action
-        //        ClickableButton = false;
-        //    }
-
-        //    // Hookup the Image Button to show the Item picture
-        //    var ItemButton = new ImageButton
-        //    {
-        //        Style = (Style)Application.Current.Resources["ImageMediumStyle"],
-        //        Source = data.ImageURI
-        //    };
-
-        //    if (ClickableButton)
-        //    {
-        //        // Add a event to the user can click the item and see more
-        //        ItemButton.Clicked += (sender, args) => ShowPopup(data);
-        //    }
-
-        //    // Put the Image Button and Text inside a layout
-        //    var ItemStack = new StackLayout
-        //    {
-        //        Padding = 3,
-        //        Style = (Style)Application.Current.Resources["ItemImageBox"],
-        //        HorizontalOptions = LayoutOptions.Center,
-        //        Children = {
-        //            ItemButton,
-        //        },
-        //    };
-
-        //    return ItemStack;
-        //}
-
-        /// <summary>
         /// Return a stack layout with the Player information inside
         /// </summary>
         /// <param name="data"></param>
@@ -311,53 +191,6 @@ namespace Game.Views
 
             return PlayerStack;
         }
-
-        /// <summary>
-        /// Show the Popup for the Item
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        //public bool ShowPopup(ItemModel data)
-        //{
-        //    PopupLoadingView.IsVisible = true;
-        //    PopupItemImage.Source = data.ImageURI;
-
-        //    PopupItemName.Text = data.Name;
-        //    PopupItemDescription.Text = data.Description;
-        //    PopupItemLocation.Text = data.Location.ToMessage();
-        //    PopupItemAttribute.Text = data.Attribute.ToMessage();
-        //    PopupItemValue.Text = " + " + data.Value.ToString();
-        //    return true;
-        //}
-
-        /// <summary>
-        /// Close the popup
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        //public void ClosePopup_Clicked(object sender, EventArgs e)
-        //{
-        //    PopupLoadingView.IsVisible = false;
-        //}
-
-        /// <summary>
-        /// Closes the Round Over Popup
-        /// 
-        /// Launches the Next Round Popup
-        /// 
-        /// Resets the Game Round
-        /// 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        //public void CloseButton_Clicked(object sender, EventArgs e)
-        //{
-        //    // Reset to a new Round
-        //    _ = BattleEngineViewModel.Instance.Engine.Round.NewRound();
-
-        //    // Show the New Round Screen
-        //    ShowModalNewRoundPage();
-        //}
 
         /// <summary>
         /// Start next Round, returning to the battle screen
