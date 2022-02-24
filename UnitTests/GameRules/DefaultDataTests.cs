c911196f54d56381e513501263d2be5bc7f24fb7
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-23 22:40:32 -0800

    Fixed unit test got failed after adding new monsters in DefaultDataTests.cs

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -49,7 +49,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(6, result.Count);
+            Assert.AreEqual(8, result.Count);
         }
 
 
