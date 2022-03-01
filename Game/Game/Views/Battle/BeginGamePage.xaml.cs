535fcda62a5909290f1f040d6f0da2709a2565e3
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-28 22:20:17 -0800

    Added unit tests for beginGamePage in BeginGamePageTests.cs and removed the unused funtions from BeginGamePage.xaml.cs

--- Game/Game/Views/Battle/BeginGamePage.xaml.cs
+++ Game/Game/Views/Battle/BeginGamePage.xaml.cs
@@ -30,20 +30,6 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// funtion to redirect player to the thank you page
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void ThankyouButtonClickedAsync(object sender, EventArgs e)
-        {
-            BattlePage thankyoupage = new BattlePage();
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleStateEnum = BattleStateEnum.GameOver;
-            await Navigation.PushModalAsync(new NavigationPage(thankyoupage));
-            _ = await Navigation.PopAsync();
-
-        }
-
-        /// <summary>
         /// Show the Game Over Screen
         /// </summary>
         /// <param name="sender"></param>
@@ -52,53 +38,6 @@ namespace Game.Views
         {
             //ShowBattleMode();
             await Navigation.PushModalAsync(new ScorePage());
-        }
-
-        /// <summary>
-        /// Directs player to the battle page with map
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void BattleMapClickedAsync(object sender, EventArgs e)
-        {
-            BattlePage mapgridpage = new BattlePage();
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleStateEnum = BattleStateEnum.Battling;
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.BattleModeEnum = BattleModeEnum.MapNext;
-            await Navigation.PushModalAsync(new NavigationPage(mapgridpage));
-            _ = await Navigation.PopAsync();
-
-        }
-
-        /// <summary>
-        /// Start next Round, returning to the battle screen
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void AutoAssignButton_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new PickItemsPage());
-        }
-
-        /// <summary>
-        /// Show the Round Over page
-        /// 
-        /// Round Over is where characters get items
-        /// 
-        /// </summary>
-        public async void RoundPage_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new RoundOverPage());
-        }
-
-        /// <summary>
-        /// button to the next round page
-        /// that displays characters and monsters
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void NextRoundPage_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new NewRoundPage());
         }
     }
 }
