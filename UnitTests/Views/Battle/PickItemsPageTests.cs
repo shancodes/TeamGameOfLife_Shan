ed97864377a6b1379aff82b33bdff1035097fdeb
Fnu Shipra <shipravalecha92@gmail.com>
2022-02-28 21:43:17 -0800

    Added Unit test for auto assign items button in PickItemsPageTests.cs. Also Resource.designer.cs file changed after adding images to projects

--- UnitTests/Views/Battle/PickItemsPageTests.cs
+++ UnitTests/Views/Battle/PickItemsPageTests.cs
@@ -196,5 +196,19 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void PickItemsPage_AutoAssignButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.AutoAssignButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
